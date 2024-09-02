2815dbb6fa00ab3f0d367fc7c336489276aad2dd
Steve <arkaine@gmx.de>
2024-09-02 22:17:36 +0200

    added working connection to mongodb

--- netlify/functions/quali_app/quali_app.js
+++ netlify/functions/quali_app/quali_app.js
@@ -8,10 +8,17 @@ const handler = async (event) => {
     try {
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection(process.env.MONGODB_COLLECTION);
-        // Function logic here ...
+        
+        // Beispiel: Abrufen aller Dokumente aus der Collection
+        const results = await collection.find({}).toArray();
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results) // Rückgabe als JSON
+        };
     } catch (error) {
-        return { statusCode: 500, body: error.toString() }
+        return { statusCode: 500, body: JSON.stringify({ error: error.toString() }) };
     }
 }
 
-module.exports = { handler }
+module.exports = { handler };
