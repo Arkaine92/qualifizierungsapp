2d760b1c14943009cffb7df9a314533c09ae761b
Steve <arkaine@gmx.de>
2024-09-02 22:07:57 +0200

    added mongodb

--- netlify/functions/quali_app/quali_app.js
+++ netlify/functions/quali_app/quali_app.js
@@ -1,17 +1,17 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
 const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    try {
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
+        // Function logic here ...
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
