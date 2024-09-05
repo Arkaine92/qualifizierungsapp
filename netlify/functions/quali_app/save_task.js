b827fc0eb7418fbb18ab302e392b8c1fbfa66891
SteveHoffmannCarl <sh@carl-group.de>
2024-09-05 17:47:15 +0200

    added save_task

---
+++ netlify/functions/quali_app/save_task.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db('qualificationapp');
+        const collection = database.collection('time_entries');
+        const data = JSON.parse(event.body);
+
+        // Füge den neuen Task in die time_entries-Collection ein
+        await collection.insertOne({
+            key: data.key,
+            company: data.company.name, // Speichere den Namen der ausgewählten Firma
+            task: data.task,
+            time: data.time,
+            description: data.description,
+            createdAt: new Date()
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ success: true })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ success: false, error: error.toString() })
+        };
+    }
+};
+
+module.exports = { handler };
